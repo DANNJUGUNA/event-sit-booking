1b9f2c4ad1d0126441caf4e9daebc703e9372b29
DANNJUGUNA <ndan30071@gmail.com>
2023-01-26 19:54:54 +0300

    addupdated it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-
+import Navbar from "./components/Navbar";
+import EventsDisplay from "./pages/EventsDisplay";
+import React,{useState,useEffect} from "react";
+import { Routes,Route } from "react-router-dom";
+import EventBookForm from "./components/EventBookForm";
+import AddEvents from "./pages/AddEvents";
 function App() {
+  const[events,setEvents]=useState([]);
+
+useEffect(()=>{
+      fetch("http://localhost:8000/events")
+     .then((response)=>response.json())
+     .then((data)=>setEvents(data))
+},[])
+const handleRerender=(event)=>{
+  setEvents([...events,event])
+}
   return (
-    <div >
+    <div className="container">
+     <Navbar/>
+     <Routes>
+     
+     <Route path='/events' element={<EventsDisplay events={events}/>}/>
+     <Route path='/EventBookForm' element={<EventBookForm/>}/>
+     <Route path='/addevents' element={<AddEvents addevent={handleRerender}/>}/>
+     </Routes>
      
     </div>
   );
