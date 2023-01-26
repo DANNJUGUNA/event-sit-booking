4832b82b62b246b21a5259de642450011892cbc4
DANNJUGUNA <ndan30071@gmail.com>
2023-01-27 00:01:07 +0300

    updated the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ()=> {
     return(
         <nav className="navbar navbar-expand-lg bg-light  ">
             <div className="container-fluid p-7">
-                <h1>EVENT-BOOK</h1>
+                <h1>EVENT-SEAT-BOOK</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
